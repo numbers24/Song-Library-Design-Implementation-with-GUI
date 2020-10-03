e972cb0799b3cbe94cbcd59703bb4c8316f24b6c
Christopher DeAngelis <45888417+numbers24@users.noreply.github.com>
2020-10-02 22:46:23 -0400

    Add files via upload

---
+++ songlib/src/app/SongLib.java
@@ -0,0 +1,32 @@
+//James Aikins & Christopher DeAngelis
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import view.SonglibController;
+
+public class SongLib extends Application {
+	
+	public void start(Stage primaryStage) throws Exception {
+		
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/view/songlib.fxml"));
+			AnchorPane root = (AnchorPane)loader.load();
+			SonglibController Controller = loader.getController();
+			Controller.start(primaryStage);
+			Scene scene = new Scene(root, 600, 400);
+			primaryStage.setScene(scene);
+			primaryStage.show(); 
+
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		launch(args);
+	}
+
+}
